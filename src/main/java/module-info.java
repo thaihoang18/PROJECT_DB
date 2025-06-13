a0dd9f51289844b41e410a0c2b4c43afa5ce2b1d
Đinh Khắc Hoàng <dinhkhachoang7a@gmail.com>
2025-06-13 08:57:06 +0700

    n

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,27 @@
 module sample.project_db {
+    // JavaFX modules
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.base;
+
+    // Java SQL and PostgreSQL
     requires java.sql;
     requires org.postgresql.jdbc;
 
-    
-    exports sample.project_db.utils.hoang to javafx.graphics;
+    // Exports
+    exports sample.project_db;
+    exports sample.project_db.controller;
+    exports sample.project_db.controller.customer;
+    exports sample.project_db.controller.admin;
+    exports sample.project_db.utils.hoang;
 
-    opens sample.project_db.utils.hoang to javafx.graphics;
-
-    opens sample.project_db.controller.admin to javafx.fxml;
-    exports sample.project_db.controller.admin to javafx.fxml;
-
- 
+    // Opens
+    opens sample.project_db to javafx.graphics, javafx.fxml;
     opens sample.project_db.model to javafx.base, javafx.fxml;
+    opens sample.project_db.DTO to javafx.base;
+    opens sample.project_db.controller to javafx.fxml;
+    opens sample.project_db.controller.customer to javafx.fxml;
+    opens sample.project_db.controller.admin to javafx.fxml;
+    opens sample.project_db.utils.hoang to javafx.graphics;
 }

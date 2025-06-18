0ea90cf329aee869dbe0bc3bd8e4c4ccb153dae6
Đinh Khắc Hoàng <dinhkhachoang7a@gmail.com>
2025-06-18 13:18:05 +0700

    edit module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,24 +4,23 @@ module sample.project_db {
     requires java.sql;
     requires javafx.graphics;
     requires javafx.base;
-    opens sample.project_db.controller.customer;
-exports sample.project_db.controller.customer to javafx.fxml;
+
+    opens sample.project_db.controller.customer to javafx.fxml;
+    exports sample.project_db.controller.customer to javafx.fxml;
 
     opens sample.project_db.model to javafx.base, javafx.fxml; 
     exports sample.project_db;
     opens sample.project_db.DTO to javafx.base;
-
-
 
     opens sample.project_db to javafx.graphics, javafx.fxml;
     
     opens sample.project_db.controller to javafx.fxml;
     exports sample.project_db.controller;
     
-    
     opens sample.project_db.controller.admin to javafx.fxml;
     exports sample.project_db.controller.admin;
 
-
-
+    // Thêm khai báo cho package utils.hoang
+    opens sample.project_db.utils.hoang to javafx.fxml; // Cho phép FXML loader truy cập
+    exports sample.project_db.utils.hoang to javafx.graphics; // Cho phép javafx.graphics truy cập
 }
